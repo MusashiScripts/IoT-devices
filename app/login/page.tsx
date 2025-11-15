3d4c71dd9471e8c35d2857048d8510117dfdcc5c
MusashiScripts <alejarteaga2003@gmail.com>
2025-11-15 00:34:46 -0500

    creating login pages

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
+      <LoginForm />
+    </section>
+  )
+}
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/components/SignUpForm";
+
+export default function SignUpPage() {
+  return (
+    <section className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
+      <SignUpForm />
+    </section>
+  )
+}
