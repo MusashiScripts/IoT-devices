e34a68e46e20f733576aeb131fa3e8884467f6ca
MusashiScripts <alejarteaga2003@gmail.com>
2025-11-15 00:40:02 -0500

    developing deviceCard

--- components/DeviceCard.tsx
+++ components/DeviceCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { Device } from '@/lib/types'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
 import { Clock, Power, Settings, Zap } from 'lucide-react'
@@ -6,15 +8,21 @@ import { Switch } from './ui/switch'
 import { Button } from './ui/button'
 import { useState } from 'react'
 import { ScheduleDialog } from './ScheduleDialog'
+import { createClient } from '@/utils/supabase/client'
+import { useRouter } from 'next/navigation'
 
 interface DeviceCardProps {
   device: Device
-  onToggle: (deviceId: string) => void
-  onSchedule: (deviceId: string, schedule: Device['schedule']) => void
+  /* onToggle: (deviceId: string) => void
+  onSchedule: (deviceId: string, schedule: Device['schedule']) => void */
 }
 
-export function DeviceCard({ device, onToggle, onSchedule }: DeviceCardProps) {
+export function DeviceCard({ device }: DeviceCardProps) {
   const [isScheduleOpen, setIsScheduleOpen] = useState(false)
+  const router = useRouter()
+
+  const supabase = createClient()
+
 
   const getDeviceIcon = (type: string) => {
     switch (type.toLowerCase()) {
@@ -34,6 +42,44 @@ export function DeviceCard({ device, onToggle, onSchedule }: DeviceCardProps) {
   const handleOpenChange = (value: boolean) => {
     setIsScheduleOpen(value)
   }
+
+  const handleDeviceToggle = async (deviceId: string) => {
+    //Perfecto, solo falta q sea en tiempo real
+
+    const now = new Date()
+    const date = new Intl.DateTimeFormat('en-CA').format(now)
+    console.log(date) // "2025-11-13"
+
+    const { data, error } = await supabase
+      .from('devices')
+      .update({ isOn: !device.isOn, lastUpdated: date })
+      .eq('device_id', deviceId)
+
+    if (data) {
+      console.log(data)
+    }
+
+    if (error) {
+      console.log(error)
+    }
+
+    //Por ahora un refresh para q se vean los cambios pero mejor usar el real-time
+    // con el metodo subscirbe al channel, el codigo esta justo debajo comentado
+
+    router.refresh()
+
+  }
+
+
+  /* const devices = supabase.channel('custom-update-channel')
+    .on(
+      'postgres_changes',
+      { event: 'UPDATE', schema: 'public', table: 'devices' },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe() */
 
   return (
     <Card className='hover:shadow-md transition-shadow'>
@@ -62,9 +108,10 @@ export function DeviceCard({ device, onToggle, onSchedule }: DeviceCardProps) {
           <div className='flex items-center gap-2'>
             <span className='text-sm text-muted-foreground'>{device.isOn ? 'Encendido' : 'Apagado'}</span>
             <Switch
+              className='cursor-pointer'
               checked={device.isOn}
               disabled={device.status === 'offline'}
-              onCheckedChange={() => onToggle(device.id)}
+              onCheckedChange={() => handleDeviceToggle(device.device_id)}
             />
           </div>
         </div>
@@ -86,12 +133,12 @@ export function DeviceCard({ device, onToggle, onSchedule }: DeviceCardProps) {
           </div>
         </div>
 
-        {device.schedule?.enabled && (
+        {/* {device.schedule?.enabled && (
           <div className='bg-blue-50 text-xs text-blue-700 p-2 rounded flex items-center gap-1 '>
             <Clock className='size-3' />
             <span>Programado: {device.schedule.action} a las {device.schedule.time}</span>
           </div>
-        )}
+        )} */}
       </CardContent>
 
       <CardFooter>
@@ -108,7 +155,7 @@ export function DeviceCard({ device, onToggle, onSchedule }: DeviceCardProps) {
         open={isScheduleOpen}
         onOpenChange={handleOpenChange}
         device={device}
-        onSchedule={onSchedule}
+      /* onSchedule={onSchedule} */
       />
 
     </Card>
