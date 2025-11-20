4b5e020decfc30fcb1b57b87de48a966b258a297
MusashiScripts <alejarteaga2003@gmail.com>
2025-11-19 22:17:45 -0500

    using router to test

--- hooks/useDevice.ts
+++ hooks/useDevice.ts
@@ -1,5 +1,6 @@
 import { Device, Schedule } from '@/lib/types'
 import { deleteScheduleById, getDeviceSchedule } from '@/services/schedules'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 
@@ -7,6 +8,7 @@ export const useDevice = (device: Device, onToggle: (deviceId: string, value: bo
   const [deviceSchedules, setDeviceSchedules] = useState<Schedule[]>([])
   const [isScheduleOpen, setIsScheduleOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
 
 
   useEffect(() => {
@@ -41,7 +43,7 @@ export const useDevice = (device: Device, onToggle: (deviceId: string, value: bo
 
     //Por ahora un refresh para q se vean los cambios pero mejor usar el real-time
 
-    //router.refresh()
+    router.refresh()
 
     //Ya esta en real-time, por eso router.refresh esta comentado
   }
